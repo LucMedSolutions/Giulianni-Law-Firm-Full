e5015d31da0421b0b09f9eaec7ff3e29be45e4c8
LucMedSolutions <lucmedsolutions@gmail.com>
2025-06-07 19:46:12 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Roger Giulani Portal',
+  description: 'Portal',
+  generator: 'N/A',
 }
 
 export default function RootLayout({
